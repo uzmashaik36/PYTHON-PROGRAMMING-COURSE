3c6f34c6a05e3ce7cd44a9d5aeb49753865667d7
UZMA BEGUM SHAIK <144657854+uzmashaik36@users.noreply.github.com>
2026-05-21 22:21:41 +0530

    Add files via upload

---
+++ chapter1/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("Hello, World!")
+
+# This is print joke from pyjokes library
+joke = pyjokes.get_joke()
+
+print(joke)
+
+"""
+this is a multi-line comment
+which can be used to write longer explanations"""
